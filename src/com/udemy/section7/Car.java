6cc8a9104af729d404824490847e3bb6d9ded5f3
dnlwlnc <daniel.welenc@gmail.com>
2017-05-17 16:13:52 +0200

    Start sekcji 7

---
+++ src/com/udemy/section7/Car.java
@@ -0,0 +1,23 @@
+package com.udemy.section7;
+
+/**
+ * Created by welencd on 2017-05-17.
+ */
+
+public class Car {
+    // public - unrestricted access to the class
+    // private - restricted access
+    // protected - available only in the package
+
+    // FIELDS:
+        // private -> encapsulation
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+    // CONSTRUCTOR
+
+
+}
---
+++ src/com/udemy/section7/Main.java
@@ -0,0 +1,13 @@
+package com.udemy.section7;
+
+/**
+ * Created by welencd on 2017-05-17.
+ */
+public class Main {
+    public static void Main(String[] args){
+        Car porsche = new Car();
+        Car holden = new Car();
+
+
+    }
+}
