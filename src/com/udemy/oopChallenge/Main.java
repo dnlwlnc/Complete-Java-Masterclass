967bfab40820f8f10ebc074b2f57427f8e4f3c2b
dnlwlnc <daniel.welenc@gmail.com>
2017-06-02 16:11:23 +0200

    OOP - mega challenge

--- src/com/udemy/oopChallenge/Main.java
+++ src/com/udemy/oopChallenge/Main.java
@@ -1,12 +1,10 @@
 package com.udemy.oopChallenge;
 
-/**
- * Created by welencd on 2017-06-02.
- */
+
 public class Main {
     public static void main(String[] args){
 
-        // The purpose of the application is to help a ficticious company called Bills Burgers to manage
+        // The purpose of the application is to help a fictitious company called Bills Burgers to manage
         // their process of selling hamburgers.
         // Our application will help Bill to select types of burgers, some of the additional items (additions) to
         // be added to the burgers and pricing.
@@ -17,7 +15,7 @@ public class Main {
         // the customer can select to be added to the burger. Each one of these items gets charged an additional
         // price so you need some way to track how many items got added and to calculate the price (for the base
         // burger and all the additions).
-        // This burger has a base price and the additions are all seperately priced.
+        // This burger has a base price and the additions are all separately priced.
         // Create a Hamburger class to deal with all the above.
         // The constructor should only include the roll type, meat and price.
         // Also create two extra varieties of Hamburgers (classes) to cater for
@@ -29,7 +27,7 @@ public class Main {
         // hint:  You have to find a way to automatically add these new additions at the time the deluxe burger
         // object is created, and then prevent other additions being made.
         //  All 3 classes should have a method that can be called anytime to show the base price of the hamburger
-        // plus all additionals, each showing the addition name, and addition price, and a grand total for the
+        // plus all additions, each showing the addition name, and addition price, and a grand total for the
         // burger
         // For the two additional classes this may require you to be looking at the base class for pricing and then
         // adding totals onto that.
